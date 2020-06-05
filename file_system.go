d20985a1b7ede3d7036727ea8c58b6ec468fd12c
Alexander Rezvov <rezvov@majordomo.ru>
2020-06-05 11:16:24 +0300

    probably fix MLST parsing

--- file_system.go
+++ file_system.go
@@ -421,7 +421,7 @@ func parseMLST(entry string, skipSelfParent bool) (os.FileInfo, error) {
 	parseError := ftpError{err: fmt.Errorf(`failed parsing MLST entry: %s`, entry)}
 	incompleteError := ftpError{err: fmt.Errorf(`MLST entry incomplete: %s`, entry)}
 
-	parts := strings.Split(entry, "; ")
+	parts := strings.SplitN(entry, "; ", 2)
 	if len(parts) != 2 {
 		return nil, parseError
 	}
